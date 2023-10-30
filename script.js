40f4b7461190ea3dbfef6fde2a001bf67c79666b
DjCr4y0n <cray.kox1234@gmail.com>
2023-10-30 20:08:22 +0100

    add functions buttons: add, substract multiply divide and style them

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var container = document.querySelector('.calculator-body');
+var numbersContainer = document.querySelector('.numbers');
+var functionsContainer = document.querySelector('.functions');
 
 for (i=0;i<10;i++)
 {
@@ -9,7 +10,19 @@ for (i=0;i<10;i++)
     button.style.height = '100px';
     button.textContent = 9-i;
     button.style.color = 'white';
-    button.style.margin = '5px';
     button.style.backgroundColor = '#1f2937';
-    container.appendChild(button);
+    numbersContainer.appendChild(button);
+}
+functionsArray = ['+','-', '*', '/', '=']
+for (i=0; i<5;i++)
+{
+    let fbutton = document.createElement('button');
+    fbutton.style.textAlign = 'center';
+    fbutton.className = 'button';
+    fbutton.style.width = '100px';
+    fbutton.style.height = '100px';
+    fbutton.style.color = 'white';
+    fbutton.textContent = functionsArray[i];
+    fbutton.style.backgroundColor = '#1f2937'
+    functionsContainer.appendChild(fbutton);
 }
